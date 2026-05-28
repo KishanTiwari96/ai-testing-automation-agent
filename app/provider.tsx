aae8ab11842db617548b7c2d90cc7074d918b427
KishanTiwari96 <kishantiwari698591@gmail.com>
2026-05-29 00:50:35 +0530

    Initial Commit

---
+++ app/provider.tsx
@@ -0,0 +1,34 @@
+"use client"
+
+import { UserDetailContext } from '@/context/UserDetailContext';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+
+function Provider({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+
+  const [userDetail,setUserDetail] = useState<any>()
+  
+  useEffect(() => {
+    CreateNewUser()
+  }, [])
+
+  const CreateNewUser = async () => {
+    const result = await axios.post('/api/users', {})
+
+    console.log("Result", result);
+    setUserDetail(result.data?.user);
+  }
+  return (
+    <div>
+      <UserDetailContext.Provider value={{userDetail, setUserDetail}}>
+        {children}
+      </UserDetailContext.Provider>
+    </div>
+  )
+}
+
+export default Provider
